a74c7a1d22b2ec1b9c6c21fc878a3b315e251bd8
siroj05 <rojudin123@gmail.com>
2025-01-21 22:57:12 +0700

    fix typo

--- src/components/auth/login/formLogin.tsx
+++ src/components/auth/login/formLogin.tsx
@@ -40,7 +40,7 @@ export default function FormLogin() {
             isDisable() ? "text-gray-600" : "text-white"
           }`}
         >
-          Daftar
+          Login Akun
         </div>
       </ButtonAuth>
     </form>
